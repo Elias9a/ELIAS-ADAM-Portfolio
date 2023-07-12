fb1cdea22c2b3281b05c5c0dab0be3d1d4db31ff
elias09a <elias8adam@gmail.com>
2023-07-12 23:43:51 +0100

    working on burger menue

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const toggle = (toggleTextId) => {
-    const toggleText = document.getElementById(toggleTextId);
-    toggleText.style.display === "none" ?
-      toggleText.style.display = "block" : toggleText.style.display = "none";
-  }
-  
-document.querySelector('.hamburger').addEventListener('click', function() {
-  document.querySelector('.nav-ul').classList.toggle('open');
+
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav .nav-ul");
+const nav = document.querySelector("nav");
+
+const navLink = document.querySelectorAll(".nav-link");
+
+burger.addEventListener("click", () => {
+  ul.classList.toggle("show");
 });
+
+navLink.forEach((link) =>
+  link.addEventListener("click", () => {
+    ul.classList.remove("show");
+  })
+);
